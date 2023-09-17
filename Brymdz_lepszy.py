371b1bd2760bc6e282090b97ba5069dd821fb724
kichal01 <145274013+kichal01@users.noreply.github.com>
2023-09-17 17:05:10 +0200

    Add files via upload

---
+++ Brymdz_lepszy.py
@@ -0,0 +1,35 @@
+from klasa1 import Deck,Card,Hand,Board
+import random
+deck=Deck()
+N= Hand()
+E= Hand()
+S= Hand()
+W= Hand()
+
+board1=Board(N,E,S,W,1)
+N.points_deal(10,20,deck)
+
+# board1.random_deal(deck)
+print('N: ')
+N.show()
+print('Liczba punktow: ' + str(N.number_of_points))
+print('Liczba kart: ' + str(N.number_of_cards))   
+print('Talia: ')
+deck.show()
+# print('E: ')
+# E.show()
+# print('Liczba punktow: ' + str(E.number_of_points))
+# print('Liczba kart: ' + str(E.number_of_cards))   
+# print('S: ')
+# S.show()
+# print('Liczba punktow: ' + str(S.number_of_points))
+# print('Liczba kart: ' + str(S.number_of_cards))   
+# print('W: ')
+# W.show()
+# print('Liczba punktow: ' + str(W.number_of_points))
+# print('Liczba kart: ' + str(W.number_of_cards))   
+
+# print('Pozostale honory: ')
+# for i in range(len(deck.honors)):
+#     print(str(deck.honors[i].colour)+str(deck.honors[i].number)+ ' ')
+
---
+++ klasa1.py
@@ -0,0 +1,140 @@
+import random
+import copy
+class Card:
+    #number = J,Q,K,A like in the deck
+    #counter - J=11,Q=12,K=13,A=14 - for easier enumerating in the future
+    #colour 1:clubs, 2:diamonds, 3:hearts, 4:spades
+    def __init__ (self,counter:int,colour:int):
+        self.counter=counter
+        
+        if (self.counter == 11):   self.number = 'J'
+        elif (self.counter == 12): self.number = 'Q'
+        elif (self.counter == 13): self.number = 'K'
+        elif (self.counter == 14): self.number = 'A'
+        else: self.number = counter
+        
+        if(colour == 1): self.colour = 'trefl'
+        if(colour == 2): self.colour = 'karo'
+        if(colour == 3): self.colour = 'kier'
+        if(colour == 4): self.colour = 'pik'
+
+    def points(self):
+        if (self.number == 'J'): return 1
+        elif (self.number == 'Q'): return 2
+        elif (self.number == 'K'): return 3
+        elif (self.number == 'A'): return 4
+        else: return 0
+    def show(self):
+        print('Moja karta to: ' + str(self.number)+' ' +str(self.colour))
+    def __eq__(self,other):
+        return self.colour==other.colour and self.number==other.number
+class Deck:
+    def __init__ (self):
+        self.deck=[]
+        self.honors=[] #additional list, it'd be helpful while dealing under particular conditions
+        self.numbers=[] 
+        for i in range (4):
+            for j in range(13):
+                c = Card(j+2,i+1)
+                self.deck.append(c)
+                if(j+2>10):self.honors.append(c)
+                else:self.numbers.append(c)
+    
+    def show(self):
+        for i in range(len(self.deck)):
+            self.deck[i].show()  
+     
+    def put_cards(self,x:list): #x is a list of random indexes which should be used to grab cards from the deck
+        s=[] #list of prepared cards
+        h=[] #list of prepared honors
+        n=[] #list of prepared numbers
+        for i in range (len(x)):
+            s.append(self.deck[x[i]])
+            if(self.deck[x[i]].counter>10):h.append(self.deck[x[i]])
+            else:n.append(self.deck[x[i]])
+        self.deck = [self.deck[i] for i in range(len(self.deck)) if i not in x]
+        for i in range(len(h)):
+           self.honors.remove(h[i])
+        for i in range(len(n)):
+           self.numbers.remove(n[i])
+        return s
+    
+    def put_honors(self,x:list): #x is a list of random indexes which should be use to grab honors from the deck
+        h=[] #list of prepared honors
+        for i in range (len(x)):
+            h.append(self.honors[x[i]])#get honor from honors list
+        self.deck = [self.deck[i] for i in range(len(self.deck)) if i not in x] #actualization of card list 
+        for i in range(len(h)):
+           self.honors.remove(h[i]) #actualization of honor list
+        return h
+    def put_numbers(self,x:list): #x is a list of random indexes which should be use to grab honors from the deck
+        n=[] #list of prepared numbers
+        for i in range (len(x)):
+            n.append(self.numbers[x[i]])#get numbers from honors list
+        self.deck = [self.deck[i] for i in range(len(self.deck)) if i not in x] #actualization of card list 
+        for i in range(len(n)):
+           self.numbers.remove(n[i]) #actualization of numbers list
+        return n 
+    def remove_distinct_cards(self,sample_cards):
+        for i in range(len(sample_cards)):
+            self.deck.remove(sample_cards[i])
+        
+
+    
+class Hand:
+    def __init__ (self):
+        self.hand = []
+        self.number_of_cards=0
+        self.number_of_points=0
+    def random_deal(self,deck:Deck):
+        num=random.sample(range(0,len(deck.deck)),13)
+        cards = deck.put_cards(num)
+        for i in range(len(cards)):
+            self.hand.append(cards[i])
+            self.number_of_cards+=1
+            self.number_of_points+=cards[i].points()
+    def show(self):
+        for i in range(len(self.hand)):
+            self.hand[i].show()
+    def copy(self,hand,board):
+        pass
+    def points_deal(self,min:int,max:int,deck:Deck):
+        pom = 0
+        deck_copy = copy.copy(deck)
+        while(pom==0):
+            sample_hand = Hand()
+            sample_hand.random_deal(deck_copy)
+            if((sample_hand.number_of_points>=min) and (sample_hand.number_of_points<=max)): pom=1
+        deck.remove_distinct_cards(sample_hand.hand)
+        cards = sample_hand.hand
+        for i in range(len(cards)):
+            self.hand.append(cards[i])
+            self.number_of_cards+=1
+            self.number_of_points+=cards[i].points() 
+
+    
+            
+
+        
+class Board:
+    def __init__ (self,N:Hand,E:Hand,S:Hand,W:Hand,n:int):
+        self.N=N
+        self.E=E
+        self.S=S
+        self.W=W
+        self.number = n
+    def random_deal(self,deck:Deck):
+        self.N.random_deal(deck)
+        self.E.random_deal(deck)
+        self.S.random_deal(deck)
+        self.W.random_deal(deck)
+    def point_deal(self,):
+        pass
+        
+    
+
+
+
+
+
+
